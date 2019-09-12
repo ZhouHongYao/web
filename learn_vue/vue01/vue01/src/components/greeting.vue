d296c685028de4144d4bd46d21bcb8dc6f87e6f4
alita <1906321518@qq.com>
2019-09-12 10:12:35 +0800

    vue01

---
+++ learn_vue/vue01/vue01/src/components/greeting.vue
@@ -0,0 +1,21 @@
+<template>
+    <h3>{{msg}}</h3>
+</template>
+
+<script>
+    export default {
+        name: "greeting",
+        // props: {
+        //     msg: String
+        // },
+        data(){
+            return{
+                msg: "greeting component`s msg"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
